f1d2b73b163248bebc6f153bb979c5d44d1b2721
Andrei Potapov <andr3008@gmail.com>
2022-01-24 12:47:39 +0200

    added deploy files

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+set -e
+
+
+npm run build
+
+
+cd dist
+
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:andr3008/test-assignment.git main
+
+
+
+cd -
